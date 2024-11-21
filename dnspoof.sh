91591ebac69dcda7fb9d0bce3a483dc538907fd2
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-11-21 17:27:58 -0300

    Update dnspoof.sh

--- dnspoof.sh
+++ dnspoof.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Exibe o banner
+echo "  _____           _  _____           "
+echo " |  __ \         | |/ ____|          "
+echo " | |  | | ___  __| | (___   ___  ___ "
+echo " | |  | |/ _ \/ _' |\___ \ / _ \/ __|"
+echo " | |__| |  __/ (_| |____) |  __/ (__ "
+echo " |_____/ \___|\__,_|_____/ \___|\___|"
+echo "                                     "
+
 # Solicita a entrada do usuário para URL, IP local e interface de rede
 read -p "Digite a URL que deseja redirecionar: " url
 read -p "Digite o IP local para onde deseja redirecionar: " iplocal
